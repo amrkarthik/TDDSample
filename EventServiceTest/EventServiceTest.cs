21896ed42a4426062bc89136411fd9abae6910d6
Nishitha Nair <nishitha@nalashaa.com>
2017-05-05 17:35:40 +0530

    Repo initialized and files added

---
+++ EventServiceTest/EventServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventServiceTest
+{
+    [TestClass]
+    public class EventServiceTest
+    {
+        [TestMethod]
+        public void GetDataTest()
+        {
+
+        }
+    }
+}
---
+++ EventServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventServiceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed4115c0-7c6b-4983-92ed-2ebd684d5a1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
